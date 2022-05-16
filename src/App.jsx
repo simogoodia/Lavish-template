a8055a2a75fabda40c0e345a8e4934866dd277ba
Simo Dida <simodida383@gmail.com>
2022-05-16 23:46:07 +0100

    Lavish template

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/services" element={<Services />} />
+        <Route exact path="/services" element={<Services />} />
         <Route path="/products" element={<Products />} />
         <Route path="/sign-up" element={<SignUp />} />
       </Routes>
